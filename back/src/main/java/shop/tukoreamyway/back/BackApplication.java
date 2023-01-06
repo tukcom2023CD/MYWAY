5d9846bf17720d122705526e5af1c3fac921fb34
Hyeonjun Park <phjppo0918@gmail.com>
2023-01-06 19:01:21 +0900

    feat: redis cache 설정

--- back/src/main/java/shop/tukoreamyway/back/BackApplication.java
+++ back/src/main/java/shop/tukoreamyway/back/BackApplication.java
@@ -2,7 +2,9 @@ package shop.tukoreamyway.back;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
+@EnableCaching
 @SpringBootApplication
 public class BackApplication {
   public static void main(String[] args) {
---
+++ back/src/main/java/shop/tukoreamyway/back/config/redis/RedisProperties.java
@@ -0,0 +1,15 @@
+package shop.tukoreamyway.back.config.redis;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+@Setter
+@Getter
+@ConfigurationProperties(prefix = "spring.redis")
+@Component
+public class RedisProperties {
+    private String host;
+    private int port;
+}
