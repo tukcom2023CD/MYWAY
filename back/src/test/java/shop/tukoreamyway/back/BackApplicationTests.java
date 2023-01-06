2f2b331763ea1d47fd876d5eafc3d208dd1cab0b
Hyeonjun Park <phjppo0918@gmail.com>
2023-01-06 19:02:35 +0900

    delete: unit test 제거

--- back/src/test/java/shop/tukoreamyway/back/BackApplicationTests.java
+++ back/src/test/java/shop/tukoreamyway/back/BackApplicationTests.java
@@ -1,11 +1,8 @@
 package shop.tukoreamyway.back;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class BackApplicationTests {
-
-  @Test
-  void contextLoads() {}
+  
 }
