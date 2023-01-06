88ccf04b8ed594d8ad4d207a94bf3567720fbd14
Hyeonjun Park <phjppo0918@gmail.com>
2023-01-06 22:18:07 +0900

    test : 샘플 코드 예시

---
+++ back/src/test/java/shop/tukoreamyway/back/sample/member/MemberController.java
@@ -0,0 +1,35 @@
+package shop.tukoreamyway.back.sample.member;
+
+//@Slf4j
+//@RestController
+//@RequiredArgsConstructor
+//@RequestMapping("api/members")
+//public class MemberController {
+//
+//    private final MemberService memberService;
+//
+//    @PostMapping
+//    public String save() {
+//        memberService.save();
+//        return "good";
+//    }
+//
+//    @GetMapping
+//    public List<Member> getAll() {
+//        long start = System.currentTimeMillis();
+//        List<Member> result = memberService.getAll();
+//        long end = System.currentTimeMillis();
+//        log.info("{}", end - start);
+//        return result;
+//    }
+//
+//    @Cacheable(value = "post-test", cacheManager = "cacheManager")
+//    @GetMapping("ch")
+//    public List<Member> getAllCh() {
+//        long start = System.currentTimeMillis();
+//        List<Member> result = memberService.getAll();
+//        long end = System.currentTimeMillis();
+//        log.info("{}", end - start);
+//        return result;
+//    }
+//}
