bf47dc282df5b376c72d410e93ee1415c61b885e
Choejeonghun <saa00123@naver.com>
2023-01-08 19:58:35 +0900

    feat: page routing

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,8 +1,23 @@
 import React from 'react';
-import './App.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Main from './pages/main/MainPage';
+import ProjectMain from './pages/project/ProjectMain';
+import SprintMain from './pages/sprint/SprintMain';
+import TaskMain from './pages/task/TaskMain';
+import TicketMain from './pages/ticket/TicketMain';
 
 function App() {
-  return <div>기본 세팅 완료</div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Main />} />
+        <Route path='/ProjectMain' element={<ProjectMain />} />
+        <Route path='/SprintMain' element={<SprintMain />} />
+        <Route path='/TaskMain' element={<TaskMain />} />
+        <Route path='/TicketMain' element={<TicketMain />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
