0abadd5a7bb5c1110cf433f6ae7595350e968d71
Choejeonghun <saa00123@naver.com>
2023-01-11 21:17:00 +0900

    feat: header 1차 세팅

--- front/src/components/Header.tsx
+++ front/src/components/Header.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 function Header() {
-  return <div className='w-[100%] h-[80px] bg-[#97BEFF]'>Header</div>;
+  return <div className='w-[100%] h-[80px] bg-[#97BEFF]' />;
 }
 
 export default Header;
