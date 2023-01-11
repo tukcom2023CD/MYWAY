39bedbc23529c93d22ecd8a0520d491dee186353
Choejeonghun <saa00123@naver.com>
2023-01-11 21:51:26 +0900

    feat:logo 위치 임시 구현

--- front/src/components/Header.tsx
+++ front/src/components/Header.tsx
@@ -1,7 +1,11 @@
 import React from 'react';
 
 function Header() {
-  return <div className='w-[100%] h-[80px] bg-[#97BEFF]' />;
+  return (
+    <div className='flex justify-left items-center w-[100%] h-[80px] bg-[#97BEFF]'>
+      <div id='logo' className='w-[70px] h-[70px] bg-blue-500 ml-[100px]' />
+    </div>
+  );
 }
 
 export default Header;
