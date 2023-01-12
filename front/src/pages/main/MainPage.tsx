8f140ea4066078ccd07e5de47b4ae73bbeccf75f
Choejeonghun <saa00123@naver.com>
2023-01-12 22:38:44 +0900

    feat:배경 색

--- front/src/pages/main/MainPage.tsx
+++ front/src/pages/main/MainPage.tsx
@@ -4,7 +4,9 @@ import Header from '../../components/Header';
 function MainPage() {
   return (
     <div>
-      <Header />
+      <div className='p-[5px] w-[100vw] h-[100vh] bg-[#0080FF]'>
+        <Header />
+      </div>
     </div>
   );
 }
