97392537f306f4e5403fd83288db6e16bbeb487f
Choejeonghun <saa00123@naver.com>
2023-01-14 22:42:43 +0900

    feat: 메인 1차 디자인

--- front/src/pages/main/MainPage.tsx
+++ front/src/pages/main/MainPage.tsx
@@ -6,7 +6,26 @@ function MainPage() {
     <div>
       <div className='w-[100vw] h-[100vh] bg-[rgb(0,128,255)]'>
         <Header />
-        <div className='m-auto mt-[30px] p-[30px] w-[93vw] h-[85vh] bg-[#E27676]' />
+        <div className='flex justify-center items-center m-auto mt-[30px] p-[30px] w-[93vw] h-[85vh] bg-[#E27676]'>
+          <button
+            type='button'
+            className='mr-[3rem] w-[15rem] h-[15rem] bg-white font-bold text-black text-[20pt]'
+          >
+            프로젝트 생성
+          </button>
+          <button
+            type='button'
+            className='mr-[3rem] w-[15rem] h-[15rem] bg-white font-bold text-black text-[20pt]'
+          >
+            일정 / 스크럼
+          </button>
+          <button
+            type='button'
+            className='mr-[3rem] w-[15rem] h-[15rem] bg-white font-bold text-black text-[20pt]'
+          >
+            그래프
+          </button>
+        </div>
       </div>
     </div>
   );
