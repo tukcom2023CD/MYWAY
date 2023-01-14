8dcf90d31e71ff60ef9ea567d58cec3a9877bbbf
Choejeonghun <saa00123@naver.com>
2023-01-14 22:25:30 +0900

    style: header 조정

--- front/src/components/Header.tsx
+++ front/src/components/Header.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 function Header() {
   return (
-    <div className='flex justify-left items-center m-auto w-[99.5vw] h-[80px] bg-[#97BEFF]'>
+    <div className='flex justify-left items-center w-[100vw] h-[80px] bg-[#97BEFF]'>
       <div id='logo' className='w-[70px] h-[70px] bg-blue-500 ml-[100px]' />
     </div>
   );
