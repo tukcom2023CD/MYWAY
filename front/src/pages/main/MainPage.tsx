3c52c260a5a232f1408bb93514847725e4d2c029
Choejeonghun <saa00123@naver.com>
2023-01-14 22:23:54 +0900

    feat:project input wrap

--- front/src/pages/main/MainPage.tsx
+++ front/src/pages/main/MainPage.tsx
@@ -4,8 +4,9 @@ import Header from '../../components/Header';
 function MainPage() {
   return (
     <div>
-      <div className='p-[5px] w-[100vw] h-[100vh] bg-[#0080FF]'>
+      <div className='w-[100vw] h-[100vh] bg-[rgb(0,128,255)]'>
         <Header />
+        <div className='m-auto mt-[30px] p-[30px] w-[93vw] h-[85vh] bg-[#E27676]' />
       </div>
     </div>
   );
