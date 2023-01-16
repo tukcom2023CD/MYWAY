8aaec76b02bac999b7f54e943928504dfb4ce904
Choejeonghun <saa00123@naver.com>
2023-01-16 14:44:37 +0900

    feat:projectmake setting

--- front/src/pages/project/ProjectMake.tsx
+++ front/src/pages/project/ProjectMake.tsx
@@ -1,7 +1,15 @@
 import React from 'react';
+import Header from '../../components/Header';
 
 function ProjectMake() {
-  return <div>ProjectMake</div>;
+  return (
+    <div>
+      <div className='w-[100vw] h-[100vh] bg-[rgb(0,128,255)]'>
+        <Header />
+        <div className='flex justify-center items-center m-auto mt-[30px] p-[30px] w-[93vw] h-[85vh] bg-[#E27676]' />
+      </div>
+    </div>
+  );
 }
 
 export default ProjectMake;
