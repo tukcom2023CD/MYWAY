8b4185356045ae791ad5c2678db23516576204a8
Choejeonghun <saa00123@naver.com>
2023-01-17 22:00:11 +0900

    style: div-> button으로 수정

--- front/src/pages/project/ProjectMake.tsx
+++ front/src/pages/project/ProjectMake.tsx
@@ -7,15 +7,24 @@ function ProjectMake() {
       <div className='w-[100vw] h-[100vh] bg-[rgb(0,128,255)]'>
         <Header />
         <div className='flex justify-center items-center m-auto mt-[30px] p-[30px] w-[93vw] h-[85vh] bg-[#E27676]'>
-          <div className='flex justify-center items-center m-auto border bold w-[10rem] h-[10rem]'>
+          <button
+            type='button'
+            className='flex justify-center items-center m-auto border bold w-[10rem] h-[10rem]'
+          >
             프로젝트 마스터 이미지
-          </div>
-          <div className='flex justify-center items-center m-auto border bold w-[10rem] h-[10rem]'>
+          </button>
+          <button
+            type='button'
+            className='flex justify-center items-center m-auto border bold w-[10rem] h-[10rem]'
+          >
             프로젝트 제목
-          </div>
-          <div className='flex justify-center items-center m-auto border bold w-[10rem] h-[10rem]'>
+          </button>
+          <button
+            type='button'
+            className='flex justify-center items-center m-auto border bold w-[10rem] h-[10rem]'
+          >
             스프린트 마스터 설정
-          </div>
+          </button>
         </div>
       </div>
     </div>
