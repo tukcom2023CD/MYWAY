649ecca10a0d579781276db93e947d5e8beb65a0
Choejeonghun <saa00123@naver.com>
2023-01-17 21:59:02 +0900

    feat: projectmakepage

--- front/src/pages/project/ProjectMake.tsx
+++ front/src/pages/project/ProjectMake.tsx
@@ -6,7 +6,17 @@ function ProjectMake() {
     <div>
       <div className='w-[100vw] h-[100vh] bg-[rgb(0,128,255)]'>
         <Header />
-        <div className='flex justify-center items-center m-auto mt-[30px] p-[30px] w-[93vw] h-[85vh] bg-[#E27676]' />
+        <div className='flex justify-center items-center m-auto mt-[30px] p-[30px] w-[93vw] h-[85vh] bg-[#E27676]'>
+          <div className='flex justify-center items-center m-auto border bold w-[10rem] h-[10rem]'>
+            프로젝트 마스터 이미지
+          </div>
+          <div className='flex justify-center items-center m-auto border bold w-[10rem] h-[10rem]'>
+            프로젝트 제목
+          </div>
+          <div className='flex justify-center items-center m-auto border bold w-[10rem] h-[10rem]'>
+            스프린트 마스터 설정
+          </div>
+        </div>
       </div>
     </div>
   );
