cc656352db51c3e636be34375b2894d826931fe7
Choejeonghun <saa00123@naver.com>
2023-01-21 14:14:58 +0900

    feat : ticketpage

--- front/src/pages/ticket/TicketMain.tsx
+++ front/src/pages/ticket/TicketMain.tsx
@@ -1,7 +1,34 @@
 import React from 'react';
+import Header from '../../components/Header';
 
 function TicketMain() {
-  return <div>TicketMain</div>;
+  return (
+    <div>
+      <div className='w-[100vw] h-[100vh] bg-[#0080FF]'>
+        <Header />
+        <div className='flex justify-center items-center m-auto mt-[30px] p-[30px] w-[93vw] h-[85vh] bg-[#6CF0E0]'>
+          <button
+            type='button'
+            className='mr-[3rem] w-[15rem] h-[15rem] bg-white font-bold text-black text-[20pt]'
+          >
+            티켓 생성
+          </button>
+          <button
+            type='button'
+            className='mr-[3rem] w-[15rem] h-[15rem] bg-white font-bold text-black text-[20pt]'
+          >
+            일정 / 스크럼
+          </button>
+          <button
+            type='button'
+            className='mr-[3rem] w-[15rem] h-[15rem] bg-white font-bold text-black text-[20pt]'
+          >
+            그래프
+          </button>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default TicketMain;
