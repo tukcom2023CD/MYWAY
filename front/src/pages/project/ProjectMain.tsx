9a44866bae412d24724f0850324a6278532c6382
Choejeonghun <saa00123@naver.com>
2023-01-21 14:11:05 +0900

    style: 코드 간소화

--- front/src/pages/project/ProjectMain.tsx
+++ front/src/pages/project/ProjectMain.tsx
@@ -4,7 +4,7 @@ import Header from '../../components/Header';
 function ProjectMain() {
   return (
     <div>
-      <div className='w-[100vw] h-[100vh] bg-[rgb(0,128,255)]'>
+      <div className='w-[100vw] h-[100vh] bg-[#0080FF]'>
         <Header />
         <div className='flex justify-center items-center m-auto mt-[30px] p-[30px] w-[93vw] h-[85vh] bg-[#E27676]'>
           <button
--- front/src/pages/scrum/ScrumPage.tsx
+++ front/src/pages/scrum/ScrumPage.tsx
@@ -1,34 +1,7 @@
 import React from 'react';
-import Header from '../../components/Header';
 
-function SprintMain() {
-  return (
-    <div>
-      <div className='w-[100vw] h-[100vh] bg-[#F1F376]'>
-        <Header />
-        <div className='flex justify-center items-center m-auto mt-[30px] p-[30px] w-[93vw] h-[85vh] bg-[#E27676]'>
-          <button
-            type='button'
-            className='mr-[3rem] w-[15rem] h-[15rem] bg-white font-bold text-black text-[20pt]'
-          >
-            프로젝트 생성
-          </button>
-          <button
-            type='button'
-            className='mr-[3rem] w-[15rem] h-[15rem] bg-white font-bold text-black text-[20pt]'
-          >
-            일정 / 스크럼
-          </button>
-          <button
-            type='button'
-            className='mr-[3rem] w-[15rem] h-[15rem] bg-white font-bold text-black text-[20pt]'
-          >
-            그래프
-          </button>
-        </div>
-      </div>
-    </div>
-  );
+function ScrumPage() {
+  return <div>ScrumPage</div>;
 }
 
-export default SprintMain;
+export default ScrumPage;
