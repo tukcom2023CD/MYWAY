136158ed78733bfb35d7eb531325ed792978690b
Hyeonjun Park <phjppo0918@gmail.com>
2023-02-02 18:54:09 +0900

    delete: 기존 사용하던 authAccount 제거

--- back/src/main/java/shop/tukoreamyway/back/domain/authaccount/entity/Role.java
+++
@@ -1,6 +0,0 @@
-package shop.tukoreamyway.back.domain.authaccount.entity;
-
-public enum Role {
-  ROLE_ADMIN,
-  ROLE_USER,
-}
--- back/src/main/java/shop/tukoreamyway/back/domain/member/entity/MemberPrivacy.java
+++
@@ -1,8 +0,0 @@
-package shop.tukoreamyway.back.domain.member.entity;
-
-import javax.persistence.Embeddable;
-
-@Embeddable
-public class MemberPrivacy {
-  private String phoneNumber;
-}
