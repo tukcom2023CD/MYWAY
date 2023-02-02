afb0486f7443948478615bfca5edd45e0e8fbc53
Hyeonjun Park <phjppo0918@gmail.com>
2023-02-02 18:54:42 +0900

    feat : Querydsl 설정 구현

---
+++ back/src/main/java/shop/tukoreamyway/back/config/database/QueryDslConfig.java
@@ -0,0 +1,15 @@
+package shop.tukoreamyway.back.config.database;
+
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.persistence.EntityManager;
+
+@Configuration
+public class QueryDslConfig {
+    @Bean
+    public JPAQueryFactory jpaQueryFactory(EntityManager em) {
+        return new JPAQueryFactory(em);
+    }
+}
