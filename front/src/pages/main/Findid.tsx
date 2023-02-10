005ad4e744899d25477db21fc69cbeff80882fe8
Choejeonghun <saa00123@naver.com>
2023-02-10 21:01:23 +0900

    feat: find ID/PW

---
+++ front/src/pages/main/Findid.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function Findid() {
+  return <div>Findid</div>;
+}
+
+export default Findid;
---
+++ front/src/pages/main/Findpassword.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function Findpassword() {
+  return <div>Findpassword</div>;
+}
+
+export default Findpassword;
