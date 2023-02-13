847b0556b0d2621373a9276280d26fdd0a488118
Choejeonghun <saa00123@naver.com>
2023-02-13 21:59:29 +0900

    feat: 진행도 및 기여도

--- front/src/components/project/ProjectContribution.tsx
+++ front/src/components/project/ProjectContribution.tsx
@@ -1,7 +1,32 @@
 import React from 'react';
+import { PieChart } from 'react-minimal-pie-chart';
 
 function ProjectContribution() {
-  return <div className='bg-white w-[500px] h-[260px] rounded-[30px]' />;
+  const data = [
+    { title: 'One', value: 36, color: '#2093D3' },
+    { title: 'null', value: 64, color: '#DBDBDB' },
+  ];
+  return (
+    <div className='bg-white w-[500px] h-[260px] rounded-[30px]'>
+      <div className='flex flex-col items-center p-4 mt-2'>
+        <p className='ml-3 mr-auto text-[20px] font-bold'>프로젝트 기여도</p>
+        <div className='flex m-auto w-[450px] h-[200px]'>
+          <div className='m-auto w-[225px] h-[200px]'>
+            <PieChart className='m-auto w-[180px]' data={data} animate />
+          </div>
+          <div className='flex flex-col m-auto w-[225px] h-[200px] p-3'>
+            <div className='mr-3 text-[30px] font-bold mb-[10px]'>36%</div>
+            <div className='flex flex-col items-left justify-center font-extralight overflow-y-auto'>
+              <p>티켓받은 테스크 이름</p>
+              <p>티켓받은 테스크 이름</p>
+              <p>티켓받은 테스크 이름</p>
+              <p>티켓받은 테스크 이름</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default ProjectContribution;
--- front/src/components/project/ProjectProgress.tsx
+++ front/src/components/project/ProjectProgress.tsx
@@ -2,7 +2,15 @@ import React from 'react';
 
 function ProjectProgress() {
   return (
-    <div className='bg-white w-[500px] h-[140px] rounded-[30px] mb-[20px]' />
+    <div className='bg-white w-[500px] h-[140px] rounded-[30px] mb-[20px]'>
+      <div className='flex items-center p-4 mt-2'>
+        <p className='ml-3 mr-auto text-[20px] font-bold'>프로젝트 진행도</p>
+        <div className='mr-3 text-[30px] font-bold'>45%</div>
+      </div>
+      <div className='m-auto w-[450px] bg-gray-200 rounded-full h-2.5'>
+        <div className='bg-blue-600 h-2.5 rounded-full w-[45%]' />
+      </div>
+    </div>
   );
 }
 
