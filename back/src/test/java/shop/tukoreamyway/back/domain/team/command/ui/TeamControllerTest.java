c016ea7cdee11da9f5b3fca8a6fdeda6419c257a
Hyeonjun Park <phjppo0918@gmail.com>
2023-02-15 22:26:02 +0900

    test: TeamController Test 작성 #67

---
+++ back/src/test/java/shop/tukoreamyway/back/domain/team/command/ui/TeamControllerTest.java
@@ -0,0 +1,48 @@
+package shop.tukoreamyway.back.domain.team.command.ui;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.ResultActions;
+import shop.tukoreamyway.back.domain.team.command.application.TeamService;
+import shop.tukoreamyway.back.domain.team.dto.TeamRequest;
+import shop.tukoreamyway.back.domain.team.entity.IndustryGroup;
+import shop.tukoreamyway.back.global.IdResponse;
+import shop.tukoreamyway.back.support.docs.RestDocumentTest;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
+import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static shop.tukoreamyway.back.support.docs.ApiDocumentUtils.getDocumentRequest;
+import static shop.tukoreamyway.back.support.docs.ApiDocumentUtils.getDocumentResponse;
+
+@WebMvcTest(TeamController.class)
+@DisplayName("TeamController에서")
+class TeamControllerTest extends RestDocumentTest {
+    @MockBean
+    private TeamService teamService;
+
+    @Test
+    @DisplayName("생성을 수행하는가")
+    void successCreate() throws Exception {
+        //given
+        TeamRequest req = new TeamRequest("삼성전자", IndustryGroup.IT);
+        IdResponse<Long> expected = new IdResponse<>(1L);
+        when(teamService.create(any())).thenReturn(expected);
+        ResultActions perform = mockMvc.perform(post("/teams")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(toRequestBody(req)));
+
+        // then
+        perform.andExpect(status().isCreated());
+
+        // docs
+        perform.andDo(print())
+                .andDo(document("create team", getDocumentRequest(), getDocumentResponse()));
+    }
+}
