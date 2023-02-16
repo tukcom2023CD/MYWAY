e64d97b715aefadd450d00f7341534eb215cace3
Choejeonghun <saa00123@naver.com>
2023-02-16 13:45:07 +0900

    feat: 1차 스프린트 발표 준비

--- front/src/pages/graph/Graph.tsx
+++ front/src/pages/graph/Graph.tsx
@@ -10,6 +10,7 @@ import {
 } from 'recharts';
 import Menu from '../../components/frames/Sidebar';
 import TaskList from '../../components/graph/TaskList';
+import TaskImg from '../../images/project.png';
 
 const data = [
   {
@@ -50,6 +51,7 @@ const data = [
 ];
 
 function Graph() {
+  const [inputs, setInputs] = useState({ 최정훈: '' });
   const [points, setPoints] = useState(data);
 
   const newData = {
@@ -60,9 +62,25 @@ function Graph() {
     염종협: 0,
   };
 
+  const newDatas = {
+    date: '2022.02.16',
+    최정훈: 45,
+    권순호: 0,
+    박현준: 0,
+    염종협: 0,
+  };
+
   const onClick = () => {
     const addPoints = points.concat(newData);
-    console.log(addPoints);
+    setPoints(addPoints);
+  };
+
+  const onRemove = () => {
+    setPoints(points.filter((newData) => newData.date !== '2022.02.16'));
+  };
+
+  const onCreate = () => {
+    const addPoints = points.concat(newDatas);
     setPoints(addPoints);
   };
 
@@ -97,14 +115,63 @@ function Graph() {
           <Line type='monotone' dataKey='박현준' stroke='#db951e' />
           <Line type='monotone' dataKey='염종협' stroke='#12bbdd' />
         </LineChart>
-        <TaskList />
-        <button
-          type='button'
-          onClick={onClick}
-          className='flex justify-center items-center mt-1 ml-[430px] w-[50px] h-[30px] bg-[#D8F1FF] rounded-full'
-        >
-          완료
-        </button>
+        <div className='border flex flex-col justify-center items-right mr-[24px] bg-white w-[500px] h-[300px] rounded-[30px]'>
+          <div className='flex justify-center items-bottom w-[500px] h-[50px] p-6 mb-3'>
+            <p className='m-auto text-[20px] font-bold'>진행중인 태스크 목록</p>
+          </div>
+          <div className='flex flex-col justify-start w-[500px] h-[400px] p-5 overflow-y-auto'>
+            <div className='flex justify-start items-center w-[440px] h-[30px] mb-2 bg-[#DBDBDB] rounded-[30px]'>
+              <img
+                className='w-[30px] h-[30px] bg-gray-400 rounded-[30px]'
+                alt='ProfileImg'
+                src={TaskImg}
+              />
+              <div className='flex justify-center items-center ml-5'>
+                태스크1
+              </div>
+              <div className='flex justify-center items-center ml-5'>30pt</div>
+              <button
+                type='button'
+                onClick={onClick}
+                className='flex justify-center items-center ml-[180px] w-[50px] h-[30px] bg-[#D8F1FF] rounded-full'
+              >
+                완료
+              </button>
+              <button
+                type='button'
+                onClick={onRemove}
+                className='flex justify-center items-center ml-auto w-[50px] h-[30px] bg-[#f09696] rounded-full'
+              >
+                취소
+              </button>
+            </div>
+            <div className='flex justify-start items-center w-[440px] h-[30px] mb-2 bg-[#DBDBDB] rounded-[30px]'>
+              <img
+                className='w-[30px] h-[30px] bg-gray-400 rounded-[30px]'
+                alt='ProfileImg'
+                src={TaskImg}
+              />
+              <div className='flex justify-center items-center ml-5'>
+                태스크2
+              </div>
+              <div className='flex justify-center items-center ml-5'>15pt</div>
+              <button
+                type='button'
+                onClick={onCreate}
+                className='flex justify-center items-center ml-[180px] w-[50px] h-[30px] bg-[#D8F1FF] rounded-full'
+              >
+                완료
+              </button>
+              <button
+                type='button'
+                onClick={onClick}
+                className='flex justify-center items-center ml-auto w-[50px] h-[30px] bg-[#f09696] rounded-full'
+              >
+                취소
+              </button>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
