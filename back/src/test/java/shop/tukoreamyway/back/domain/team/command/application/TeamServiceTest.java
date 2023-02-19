e74e07e368489e3dd0f8f0022e1bda226996b9c7
Hyeonjun Park <phjppo0918@gmail.com>
2023-02-19 16:19:14 +0900

    test: TeamService Test 작성 #68

---
+++ back/src/test/java/shop/tukoreamyway/back/domain/team/command/application/TeamServiceTest.java
@@ -0,0 +1,42 @@
+package shop.tukoreamyway.back.domain.team.command.application;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import shop.tukoreamyway.back.domain.team.dto.TeamRequest;
+import shop.tukoreamyway.back.domain.team.entity.IndustryGroup;
+import shop.tukoreamyway.back.domain.team.entity.Team;
+import shop.tukoreamyway.back.domain.team.query.application.TeamQueryRepository;
+import shop.tukoreamyway.back.global.IdResponse;
+import shop.tukoreamyway.back.support.LoginTest;
+import shop.tukoreamyway.back.support.database.EnableDataBaseTest;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@EnableDataBaseTest
+@DisplayName("TeamService 에서")
+class TeamServiceTest extends LoginTest {
+    @Autowired
+    private TeamService teamService;
+    @Autowired
+    private TeamQueryRepository teamQueryRepository;
+
+    @Nested
+    @DisplayName("create 호출 시")
+    class CallCreate {
+        @Test
+        @DisplayName("저장을 수행하는가")
+        void successSave() {
+            //given
+            TeamRequest req = new TeamRequest("삼성전자", IndustryGroup.IT);
+            //when
+            IdResponse<Long> res = teamService.create(req);
+            Team team = teamQueryRepository.findById(res.getId()).get();
+            //then
+            assertThat(team.getName()).isEqualTo(req.getName());
+            assertThat(team.getIndustryGroup()).isEqualTo(req.getIndustryGroup());
+            assertThat(team.getLeader()).isEqualTo(loginUser);
+        }
+    }
+}
