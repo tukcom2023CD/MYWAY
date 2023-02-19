23e1303afd8571dc5a0c50f811d3945211d3f2ea
Choejeonghun <saa00123@naver.com>
2023-02-19 22:56:05 +0900

    feat: 소셜 로그인 링크 이동 설정

--- front/src/pages/main/Login.tsx
+++ front/src/pages/main/Login.tsx
@@ -1,10 +1,15 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 import Kakao from '../../images/kakaoIcon.png';
 import Naver from '../../images/naverIcon.png';
 import Google from '../../images/googleIcon.png';
 
 function Login() {
   const baseUrl = process.env.REACT_APP_BASE_URL;
+  const NAVER_AUTH_URL = `${baseUrl}/auth/login/naver`;
+  const GOOGLE_AUTH_URL = `${baseUrl}/auth/login/google`;
+  const KAKAO_AUTH_URL = `${baseUrl}/auth/login/kakao`;
+
   return (
     <div className='flex flex-col justify-center items-center m-auto w-[1440px] h-[960px] bg-[#31373b]'>
       <div className='flex justify-center items-center mb-[600px] w-[500px] h-[150px] text-[50pt] font-bold text-white'>
@@ -16,9 +21,28 @@ function Login() {
             SNS로 간편하게 시작하세요
           </p>
           <div className='flex justify-between items-center w-[400px] h-[100px] '>
-            <img className='w-[70px] h-[70px] ' alt='naverIcon' src={Naver} />
-            <img className='w-[70px] h-[70px] ' alt='googleIcon' src={Google} />
-            <img className='w-[70px] h-[70px] ' alt='kakaoIcon' src={Kakao} />
+            <NavLink
+              className='flex items-center p-2 space-x-3 rounded-xl'
+              to='/'
+            >
+              <img className='w-[70px] h-[70px] ' alt='naverIcon' src={Naver} />
+            </NavLink>
+            <NavLink
+              className='flex items-center p-2 space-x-3 rounded-xl'
+              to='/'
+            >
+              <img
+                className='w-[70px] h-[70px] '
+                alt='googleIcon'
+                src={Google}
+              />
+            </NavLink>
+            <NavLink
+              className='flex items-center p-2 space-x-3 rounded-xl'
+              to='/'
+            >
+              <img className='w-[70px] h-[70px] ' alt='kakaoIcon' src={Kakao} />
+            </NavLink>
           </div>
         </div>
       </div>
