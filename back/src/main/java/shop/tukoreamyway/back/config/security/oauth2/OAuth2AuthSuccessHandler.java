ddef275ace505e09ad2c9fac3e1446cf78778818
Hyeonjun Park <phjppo0918@gmail.com>
2023-02-22 19:47:57 +0900

    fix: redirectStrategy 사용

--- back/src/main/java/shop/tukoreamyway/back/config/security/oauth2/OAuth2AuthSuccessHandler.java
+++ back/src/main/java/shop/tukoreamyway/back/config/security/oauth2/OAuth2AuthSuccessHandler.java
@@ -1,19 +1,16 @@
 package shop.tukoreamyway.back.config.security.oauth2;
 
 import lombok.RequiredArgsConstructor;
-
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.web.authentication.SimpleUrlAuthenticationSuccessHandler;
 import org.springframework.stereotype.Component;
-
 import shop.tukoreamyway.back.config.security.jwt.JwtSetupService;
-
-import java.io.IOException;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
 /**
  * OAuth2 사용지 인증 성공시 진행하는 Handler
@@ -34,6 +31,6 @@ public class OAuth2AuthSuccessHandler extends SimpleUrlAuthenticationSuccessHand
             throws IOException, ServletException {
         LoginUser loginUser = (LoginUser) authentication.getPrincipal();
         jwtSetupService.addJwtTokensInCookie(response, loginUser);
-        response.sendRedirect(clientUrl);
+        getRedirectStrategy().sendRedirect(request, response, clientUrl);
     }
 }
