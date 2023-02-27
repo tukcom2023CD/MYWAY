ca8cde342f3258e124f4047bd37948a2d2a1193a
Choejeonghun <saa00123@naver.com>
2023-02-27 15:54:51 +0900

    fix: 스프린트 구성 변경

--- front/src/components/project/SprintList.tsx
+++ front/src/components/project/SprintList.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import SprintMaster from './SprintMater';
+import Sprints from '../frames/Sprint';
 
 function SprintList() {
   return (
@@ -7,8 +8,13 @@ function SprintList() {
       <div className='flex justify-center items-bottom w-[500px] h-[50px] p-6 mb-3'>
         <p className='mr-auto p-2 text-[24px] font-bold'>스프린트</p>
       </div>
-      <div className='border flex flex-col justify-start w-[500px] h-[400px] p-5 overflow-y-auto'>
+      <div className='flex flex-col justify-start w-[500px] h-[200px] p-5'>
         <SprintMaster />
+      </div>
+      <div className='flex flex-col justify-center items-center m-auto w-[500px] h-[400px] overflow-y-auto overflow-x-hidden'>
+        <Sprints />
+        <Sprints />
+        <Sprints />
       </div>
     </div>
   );
