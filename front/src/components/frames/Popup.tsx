b8338b32df54ec882aee4da6b184c50c0f02a1fe
Choejeonghun <saa00123@naver.com>
2023-03-01 21:56:22 +0900

    feat: popup 박스 구성

---
+++ front/src/components/frames/Popup.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Popup() {
+  return (
+    <div className='flex flex-col justify-center items-right bg-white w-[500px] h-[420px] rounded-[30px]'>
+      <div className='flex justify-center items-bottom w-[500px] h-[50px] p-6 mb-3'>
+        <p className='mr-auto text-[20px] font-bold'>Popup</p>
+      </div>
+    </div>
+  );
+}
+
+export default Popup;
