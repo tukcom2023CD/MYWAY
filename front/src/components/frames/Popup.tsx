e521b51411dc5019f2ef9aabe9e8c56c5d764c1f
Choejeonghun <saa00123@naver.com>
2023-03-06 22:13:15 +0900

    feat: components test page

--- front/src/components/frames/Popup.tsx
+++ front/src/components/frames/Popup.tsx
@@ -5,7 +5,7 @@ import Member from '../../images/members.png';
 
 function Popup() {
   return (
-    <div className='flex flex-col justify-center items-right bg-white w-[500px] h-[420px] rounded-[30px]'>
+    <div className='border flex flex-col justify-center items-right bg-white w-[500px] h-[420px] rounded-[30px]'>
       <div className='flex justify-center items-bottom w-[500px] h-[50px] p-6 mb-3'>
         <p className='mr-auto text-[20px] font-bold'>Popup</p>
         <div className='flex justify-center items-center'>
