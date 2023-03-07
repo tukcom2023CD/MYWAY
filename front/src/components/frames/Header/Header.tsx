4eabab4b0284bbc570529cd1f576507de66d666a
Choejeonghun <saa00123@naver.com>
2023-03-07 21:37:01 +0900

    feat: header logo

--- front/src/components/frames/Header/Header.tsx
+++ front/src/components/frames/Header/Header.tsx
@@ -1,7 +1,11 @@
 import React from 'react';
 
 function Header() {
-  return <div>Header</div>;
+  return (
+    <div className='border p-5 bg-white'>
+      <p className='text-[20px] font-bold ml-[50px]'>CreWship</p>
+    </div>
+  );
 }
 
 export default Header;
--- front/src/pages/main/WelcomePage.tsx
+++ front/src/pages/main/WelcomePage.tsx
@@ -1,7 +1,12 @@
 import React from 'react';
+import Header from '../../components/frames/Header/Header';
 
 function WelcomePage() {
-  return <div>WelcomePage</div>;
+  return (
+    <div>
+      <Header />
+    </div>
+  );
 }
 
 export default WelcomePage;
