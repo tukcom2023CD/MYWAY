0e98a5ab1a5bd8db3bdf616aea8e27b054ad8066
Choejeonghun <saa00123@naver.com>
2023-03-08 19:47:12 +0900

    feat: header fix

--- front/src/components/frames/Welcome/Header.tsx
+++ front/src/components/frames/Welcome/Header.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 function Header() {
   return (
-    <div className='border p-5 bg-white'>
+    <div className='fixed border w-full p-5 bg-white'>
       <p className='text-[20px] font-bold ml-[50px]'>CreWship</p>
     </div>
   );
