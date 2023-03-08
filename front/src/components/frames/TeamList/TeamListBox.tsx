a9c03acd97cb1e86541dac13f29cf9fcd26362bb
Choejeonghun <saa00123@naver.com>
2023-03-08 20:14:49 +0900

    feat: team box frame

--- front/src/components/frames/TeamList/TeamListBox.tsx
+++ front/src/components/frames/TeamList/TeamListBox.tsx
@@ -1,9 +1,12 @@
 import React from 'react';
+import Teams from './Teams';
 
 function TeamListBox() {
   return (
-    <div className='border flex flex-col justfiy-center items-center m-auto w-[500px] h-[600px] overflow-auto'>
-      <div className='border flex flex-col justify-center items-center w-full h-[500px]' />
+    <div className='border flex flex-col justfiy-center items-center m-auto w-[500px] h-[600px]'>
+      <div className='border flex flex-col mb-5 w-full h-[500px] overflow-y-auto'>
+        <Teams />
+      </div>
       <button
         type='button'
         className='flex justify-center items-center ml-auto w-[100px] h-[40px] rounded-[16px] bg-[#1AAAFB] text-white'
---
+++ front/src/components/frames/TeamList/Teams.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Teams() {
+  return (
+    <div className='border flex justify-start items-center w-[500px] h-[50px]'>
+      Teams
+    </div>
+  );
+}
+
+export default Teams;
