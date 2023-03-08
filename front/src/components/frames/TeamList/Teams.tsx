5312f348d0095aef384cc790c1d1a7dbb6eaf8dd
Choejeonghun <saa00123@naver.com>
2023-03-08 22:32:54 +0900

    feat: teamlist teamname get 1차

--- front/src/components/frames/TeamList/Teams.tsx
+++ front/src/components/frames/TeamList/Teams.tsx
@@ -1,9 +1,28 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
+export type TeamData = {
+  id: number;
+  nickname: string;
+  rank: string;
+  isAcceptMember: boolean;
+  isAcceptTeam: boolean;
+  name: string;
+  industryGroup: string;
+};
 
 function Teams() {
+  const [teamData, setTeamData] = useState<TeamData[] | null>();
+
+  useEffect(() => {
+    axios.get('staffs/myteam').then((response) => {
+      setTeamData(response.data);
+    });
+  }, []);
+
   return (
     <div className='border flex justify-start items-center w-[500px] h-[50px]'>
-      Teams
+      {teamData ? teamData.map((team) => <p>{team.name}</p>) : null}
     </div>
   );
 }
