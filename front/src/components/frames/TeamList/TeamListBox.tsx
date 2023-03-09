c8263d2b7f5adb268056e315b3121d0cb93a1d71
Choejeonghun <saa00123@naver.com>
2023-03-09 16:47:11 +0900

    style: teamlistpage design

--- front/src/components/frames/TeamList/TeamListBox.tsx
+++ front/src/components/frames/TeamList/TeamListBox.tsx
@@ -1,18 +1,30 @@
 import React from 'react';
 import Teams from './Teams';
+import rightarrow from '../../../images/rightarrow.png';
 
 function TeamListBox() {
   return (
-    <div className='border flex flex-col justfiy-center items-center m-auto w-[500px] h-[600px]'>
-      <div className='border flex flex-col mb-5 w-full h-[500px] overflow-y-auto'>
+    <div className='flex flex-col justfiy-center items-center m-auto w-[500px] h-[700px]'>
+      <p className='font-bold text-[30px] mb-5'>CreWship</p>
+      <div className='flex justify-between w-full mb-5'>
+        <div className='border w-[300px] h-full text-[20px] font-bold text-center pt-1'>
+          선택한 항목 없음
+        </div>
+        <button
+          type='button'
+          className='flex justify-center items-center w-[100px] h-[40px] rounded-[999px] bg-[#1AAAFB] text-white'
+        >
+          열기
+          <img
+            className='w-[24px] h-[24px] ml-2 color-white'
+            alt='rightarrow'
+            src={rightarrow}
+          />
+        </button>
+      </div>
+      <div className='border flex flex-col mb-5 w-full h-[500px] rounded-[30px] shadow-md overflow-y-auto'>
         <Teams />
       </div>
-      <button
-        type='button'
-        className='flex justify-center items-center ml-auto w-[100px] h-[40px] rounded-[16px] bg-[#1AAAFB] text-white'
-      >
-        팀 생성하기
-      </button>
     </div>
   );
 }
