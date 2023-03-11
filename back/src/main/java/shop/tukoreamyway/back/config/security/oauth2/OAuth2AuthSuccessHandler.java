9ad1c81c6cdd22ab3b99bea2c74806d2a208e89f
Hyeonjun Park <phjppo0918@gmail.com>
2023-03-11 12:17:32 +0900

    feat: frontend redirect endpoint 설정 #146

--- back/src/main/java/shop/tukoreamyway/back/config/security/oauth2/OAuth2AuthSuccessHandler.java
+++ back/src/main/java/shop/tukoreamyway/back/config/security/oauth2/OAuth2AuthSuccessHandler.java
@@ -1,19 +1,16 @@
 package shop.tukoreamyway.back.config.security.oauth2;
 
 import lombok.RequiredArgsConstructor;
-
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.web.authentication.SimpleUrlAuthenticationSuccessHandler;
 import org.springframework.stereotype.Component;
-
 import shop.tukoreamyway.back.config.security.jwt.JwtSetupService;
-
-import java.io.IOException;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
 /**
  * OAuth2 사용지 인증 성공시 진행하는 Handler
@@ -28,12 +25,15 @@ public class OAuth2AuthSuccessHandler extends SimpleUrlAuthenticationSuccessHand
     @Value("${client.url}")
     private String clientUrl;
 
+    @Value("${client.endpoint}")
+    private String endpoint;
+
     @Override
     public void onAuthenticationSuccess(
             HttpServletRequest request, HttpServletResponse response, Authentication authentication)
             throws IOException, ServletException {
         LoginUser loginUser = (LoginUser) authentication.getPrincipal();
         jwtSetupService.addJwtTokensInCookie(response, loginUser);
-        getRedirectStrategy().sendRedirect(request, response, clientUrl);
+        getRedirectStrategy().sendRedirect(request, response, clientUrl + "/" + endpoint);
     }
 }
