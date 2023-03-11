3792a42088e370d0cc993c7e138676d412fe7ee4
Hyeonjun Park <phjppo0918@gmail.com>
2023-03-11 12:17:59 +0900

    fix : task 목록 조회시 sprint fetch join 하도록 수정

--- back/src/main/java/shop/tukoreamyway/back/domain/task/query/infra/TaskQueryRepositoryImpl.java
+++ back/src/main/java/shop/tukoreamyway/back/domain/task/query/infra/TaskQueryRepositoryImpl.java
@@ -1,14 +1,9 @@
 package shop.tukoreamyway.back.domain.task.query.infra;
 
-import static shop.tukoreamyway.back.domain.task.entity.QTask.task;
-
 import com.querydsl.core.types.Predicate;
 import com.querydsl.jpa.impl.JPAQueryFactory;
-
 import lombok.RequiredArgsConstructor;
-
 import org.springframework.stereotype.Repository;
-
 import shop.tukoreamyway.back.domain.task.dto.TaskSearchCondition;
 import shop.tukoreamyway.back.domain.task.entity.Task;
 import shop.tukoreamyway.back.domain.task.query.application.TaskQueryRepositoryCustom;
@@ -17,6 +12,9 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
+import static shop.tukoreamyway.back.domain.sprint.entity.QSprint.sprint;
+import static shop.tukoreamyway.back.domain.task.entity.QTask.task;
+
 @Repository
 @RequiredArgsConstructor
 public class TaskQueryRepositoryImpl implements TaskQueryRepositoryCustom {
@@ -24,7 +22,11 @@ public class TaskQueryRepositoryImpl implements TaskQueryRepositoryCustom {
 
     @Override
     public List<Task> findAllByCondition(final TaskSearchCondition condition) {
-        return jpaQueryFactory.selectFrom(task).where(generateWhereQuery(condition)).fetch();
+        return jpaQueryFactory
+                .selectFrom(task)
+                .where(generateWhereQuery(condition))
+                .leftJoin(task.sprint, sprint).fetchJoin()
+                .fetch();
     }
 
     private Predicate[] generateWhereQuery(final TaskSearchCondition condition) {
