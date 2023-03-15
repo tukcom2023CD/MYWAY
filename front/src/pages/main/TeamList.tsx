62ea4f8fd361e3b6e229114eacee47d7580f9278
Choejeonghun <saa00123@naver.com>
2023-03-15 21:29:13 +0900

    test: react-modal test

--- front/src/pages/main/TeamList.tsx
+++ front/src/pages/main/TeamList.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import TeamListBox from '../../components/frames/TeamList/TeamListBox';
+import Modal from '../../components/Popup/TeamPopup';
 
 function TeamList() {
   return (
     <div className='flex flex-col justify-center items-center m-auto w-[100vw] h-[100vh]'>
       <TeamListBox />
+      <Modal />
     </div>
   );
 }
