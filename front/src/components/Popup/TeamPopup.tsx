725fd7a6fb3f86cc130aff4b85fb2db60ef7025a
Choejeonghun <saa00123@naver.com>
2023-03-16 17:59:22 +0900

    fix: popup -> modal

--- front/src/components/Popup/TeamPopup.tsx
+++ front/src/components/Popup/TeamPopup.tsx
@@ -1,8 +1,12 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
 import Modal from 'react-modal';
+import axios from 'axios';
 
 function TeamPopup() {
   const [modalIsOpen, setIsOpen] = useState(false);
+  const [name, setName] = useState();
+  const [industryGroup, setIndustryGroup] = useState();
 
   function openModal() {
     setIsOpen(true);
@@ -12,23 +16,80 @@ function TeamPopup() {
     setIsOpen(false);
   }
 
+  const teamData = {
+    name: `${name}`,
+    industryGroup: `${industryGroup}`,
+  };
+
+  const navigate = useNavigate();
+
+  const handleSubmit = useEffect(() => {
+    axios
+      .post(`teams`, teamData, {
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      })
+      .then((response) => {
+        console.log(response);
+        navigate('/TeamList');
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
+
   return (
     <div>
-      <button type='button' onClick={openModal}>
-        Open Modal
+      <button
+        type='button'
+        className='flex justify-center items-center ml-auto w-[100px] h-[40px] rounded-[999px] bg-[#1AAAFB] text-white'
+        onClick={openModal}
+      >
+        팀 생성하기
       </button>
-      <Modal isOpen={modalIsOpen} contentLabel='Example Modal'>
-        <button type='button' onClick={closeModal}>
-          close
-        </button>
-        <div>I am a modal</div>
-        <form>
-          <input />
-          <button type='button'>tab navigation</button>
-          <button type='button'>stays</button>
-          <button type='button'>inside</button>
-          <button type='button'>the modal</button>
-        </form>
+      <Modal
+        isOpen={modalIsOpen}
+        className='w-[100vw] h-[100vh] flex justify-center items-center fixed bg-[rgba(0,0,0,0.2)]'
+        contentLabel='Example Modal'
+      >
+        <div>
+          <div className='border flex flex-col justify-center items-center m-auto bg-white w-[480px] h-[600px] rounded-[30px]'>
+            <p className='font-bold text-[25px] p-7'>팀 생성</p>
+            <div className='mb-4 w-[430px]'>
+              <p className='font-bold text-[20px]'>제목</p>
+              <input
+                className='text-[20px] w-[430px] border-b-2'
+                onChange={(e) => setName((e.target as any).value)}
+                placeholder='제목을 입력해주세요.'
+              />
+            </div>
+            <div className='mb-4 w-[430px]'>
+              <p className='font-bold text-[20px]'>부서명</p>
+              <input
+                className='text-[20px] w-[430px] border-b-2'
+                onChange={(e) => setIndustryGroup((e.target as any).value)}
+                placeholder='부서명을 입력하세요.'
+              />
+            </div>
+            <div className='flex space-x-2'>
+              <button
+                type='button'
+                onClick={() => handleSubmit}
+                className='flex justify-center items-center w-[100px] h-[40px] rounded-[30px] bg-[#0075FF] text-white text-[12px]'
+              >
+                생성하기
+              </button>
+              <button
+                type='button'
+                onClick={closeModal}
+                className='flex justify-center items-center w-[100px] h-[40px] rounded-[30px] bg-[#0075FF] text-white text-[12px]'
+              >
+                닫기
+              </button>
+            </div>
+          </div>
+        </div>
       </Modal>
     </div>
   );
