6dfce4c8734b73d0c05a86bb0e63833e81d247f0
Soon Ho Kwon <74086829+tnsgh9603@users.noreply.github.com>
2023-03-29 14:10:12 +0900

    tag fix

--- back/src/main/java/shop/tukoreamyway/back/domain/question/command/application/QuestionService.java
+++ back/src/main/java/shop/tukoreamyway/back/domain/question/command/application/QuestionService.java
@@ -4,11 +4,13 @@ import lombok.RequiredArgsConstructor;
 
 import shop.tukoreamyway.back.domain.ability.command.application.AbilityService;
 import shop.tukoreamyway.back.domain.ability.dto.AbilityRequest;
+import shop.tukoreamyway.back.domain.ability.entity.Ability;
 import shop.tukoreamyway.back.domain.ability.entity.AbilityCategory;
 import shop.tukoreamyway.back.domain.ability.entity.GrantLocation;
 import shop.tukoreamyway.back.domain.question.dto.QuestionRequest;
 import shop.tukoreamyway.back.domain.question.dto.UpdateQuestionRequest;
 import shop.tukoreamyway.back.domain.question.entity.Question;
+import shop.tukoreamyway.back.domain.question.entity.Tag;
 import shop.tukoreamyway.back.domain.question.mapper.QuestionMapper;
 import shop.tukoreamyway.back.domain.question.query.application.QuestionQueryRepository;
 import shop.tukoreamyway.back.domain.staff.entity.Staff;
@@ -17,10 +19,14 @@ import shop.tukoreamyway.back.domain.team.entity.Team;
 import shop.tukoreamyway.back.global.IdResponse;
 import shop.tukoreamyway.back.global.service.CommandService;
 import shop.tukoreamyway.back.global.service.EntityLoader;
+import shop.tukoreamyway.back.domain.question.entity.Tag;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 import javax.persistence.EntityNotFoundException;
+
+import static org.yaml.snakeyaml.tokens.Token.ID.Tag;
 
 @CommandService
 @RequiredArgsConstructor
@@ -35,6 +41,7 @@ public class QuestionService {
     public IdResponse<Long> create(final QuestionRequest dto) {
         final Team team = teamLoader.getEntity(dto.getTeamId());
         final Staff writer = staffLoader.getActiveStaff(dto.getTeamId());
+
         final Question question =
                 questionRepository.save(questionMapper.toEntity(dto, team, writer));
         abilityService.create(
