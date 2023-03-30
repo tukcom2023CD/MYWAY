4bbc81cbd55f7bb5ab821f61b54e4a4198320fb3
Choejeonghun <saa00123@naver.com>
2023-03-30 17:52:57 +0900

    chore: pivot library

--- front/src/pages/anaylsis/Analysis.tsx
+++ front/src/pages/anaylsis/Analysis.tsx
@@ -1,25 +1,13 @@
 import React from 'react';
 import AnalysisGraph from '../../components/graph/AnalysisGraph';
 import Menu from '../../components/frames/Side/Sidebar';
-import Profile from '../../components/frames/Profile';
-import CheckList from '../../components/main/CheckListBox';
-import Progress from '../../components/main/ProjectProgress';
-import Contribution from '../../components/main/ProjectContribution';
-import Schedule from '../../components/project/Schedule';
 
 function Analysis() {
   return (
     <div className='flex m-auto w-[1440px] h-[960px] bg-[#D8F1FF]'>
       <Menu />
       <div className='flex flex-col justfiy-center items-center m-auto w-[1024px] h-[800px]'>
-        <Profile />
-        <CheckList />
         <div className='flex justify-center items-center m-auto'>
-          <div className='flex flex-col justify-center items-left w-[512px] h-[450px]'>
-            <Progress />
-            <Contribution />
-          </div>
-          <Schedule />
           <AnalysisGraph />
         </div>
       </div>
