f461264f845c254078075de898e6ea224f098827
Choejeonghun <saa00123@naver.com>
2023-04-06 00:42:23 +0900

    test: submit

--- front/src/components/Popup/TeamPopup.tsx
+++ front/src/components/Popup/TeamPopup.tsx
@@ -83,7 +83,7 @@ function TeamPopup() {
             <div className='flex space-x-2'>
               <button
                 type='button'
-                onClick={() => handleSubmit}
+                onClick={() => onSubmit}
                 className='flex justify-center items-center w-[100px] h-[40px] rounded-[30px] bg-[#0075FF] text-white text-[12px]'
               >
                 생성하기
