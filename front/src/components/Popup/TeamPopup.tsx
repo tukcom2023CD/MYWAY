1da5ce93ca19d947e44ac9cc20a7b7f540203fa6
Choejeonghun <saa00123@naver.com>
2023-04-06 18:20:37 +0900

    feat: value값 추가

--- front/src/components/Popup/TeamPopup.tsx
+++ front/src/components/Popup/TeamPopup.tsx
@@ -1,12 +1,10 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import Modal from 'react-modal';
 import axios from 'axios';
 
 function TeamPopup() {
   const [modalIsOpen, setIsOpen] = useState(false);
-  const [name, setName] = useState();
-  const [industryGroup, setIndustryGroup] = useState();
   const [data, setData] = useState({
     name: '',
     industryGroup: '',
@@ -20,28 +18,7 @@ function TeamPopup() {
     setIsOpen(false);
   }
 
-  // const teamData = {
-  //   name: `${name}`,
-  //   industryGroup: `${industryGroup}`,
-  // };
-
   const navigate = useNavigate();
-
-  // const handleSubmit = useEffect(() => {
-  //   axios
-  //     .post(`teams`, teamData, {
-  //       headers: {
-  //         'Content-Type': 'application/json',
-  //       },
-  //     })
-  //     .then((response) => {
-  //       console.log(response);
-  //       navigate('/TeamList');
-  //     })
-  //     .catch((error) => {
-  //       console.log(error);
-  //     });
-  // }, []);
 
   const handleChange = (e: {
     target: {
@@ -91,6 +68,7 @@ function TeamPopup() {
               <input
                 className='text-[20px] w-[430px] border-b-2'
                 name='name'
+                value={data.name}
                 onChange={() => handleChange}
                 placeholder='제목을 입력해주세요.'
               />
@@ -102,6 +80,7 @@ function TeamPopup() {
               <input
                 className='text-[20px] w-[430px] border-b-2'
                 name='industryGroup'
+                value={data.industryGroup}
                 onChange={() => handleChange}
                 placeholder='부서명을 입력하세요.'
               />
@@ -124,49 +103,6 @@ function TeamPopup() {
           </div>
         </form>
       </Modal>
-      {/* <Modal
-        isOpen={modalIsOpen}
-        className='w-[100vw] h-[100vh] flex justify-center items-center fixed bg-[rgba(0,0,0,0.2)]'
-        contentLabel='Example Modal'
-      >
-        <div>
-          <div className='border flex flex-col justify-center items-center m-auto bg-white w-[480px] h-[600px] rounded-[30px]'>
-            <p className='font-bold text-[25px] p-7'>팀 생성</p>
-            <div className='mb-4 w-[430px]'>
-              <p className='font-bold text-[20px]'>제목</p>
-              <input
-                className='text-[20px] w-[430px] border-b-2'
-                onChange={() => setName}
-                placeholder='제목을 입력해주세요.'
-              />
-            </div>
-            <div className='mb-4 w-[430px]'>
-              <p className='font-bold text-[20px]'>부서명</p>
-              <input
-                className='text-[20px] w-[430px] border-b-2'
-                onChange={() => setIndustryGroup}
-                placeholder='부서명을 입력하세요.'
-              />
-            </div>
-            <div className='flex space-x-2'>
-              <button
-                type='button'
-                // onClick={() => handleSubmit}
-                className='flex justify-center items-center w-[100px] h-[40px] rounded-[30px] bg-[#0075FF] text-white text-[12px]'
-              >
-                생성하기
-              </button>
-              <button
-                type='button'
-                onClick={closeModal}
-                className='flex justify-center items-center w-[100px] h-[40px] rounded-[30px] bg-[#0075FF] text-white text-[12px]'
-              >
-                닫기
-              </button>
-            </div>
-          </div>
-        </div>
-      </Modal> */}
     </div>
   );
 }
