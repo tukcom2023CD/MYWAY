7fbbcc3edd2077b88ad7f9d0fe9920d425bde825
Choejeonghun <saa00123@naver.com>
2023-04-06 18:09:36 +0900

    fix: modal fix

--- front/src/components/Popup/TeamPopup.tsx
+++ front/src/components/Popup/TeamPopup.tsx
@@ -7,6 +7,10 @@ function TeamPopup() {
   const [modalIsOpen, setIsOpen] = useState(false);
   const [name, setName] = useState();
   const [industryGroup, setIndustryGroup] = useState();
+  const [data, setData] = useState({
+    name: '',
+    industryGroup: '',
+  });
 
   function openModal() {
     setIsOpen(true);
@@ -16,36 +20,52 @@ function TeamPopup() {
     setIsOpen(false);
   }
 
-  const teamData = {
-    name: `${name}`,
-    industryGroup: `${industryGroup}`,
-  };
+  // const teamData = {
+  //   name: `${name}`,
+  //   industryGroup: `${industryGroup}`,
+  // };
 
   const navigate = useNavigate();
 
-  const handleSubmit = useEffect(() => {
-    axios
-      .post(`teams`, teamData, {
-        headers: {
-          'Content-Type': 'application/json',
-        },
-      })
-      .then((response) => {
-        console.log(response);
-        navigate('/TeamList');
-      })
-      .catch((error) => {
-        console.log(error);
-      });
-  }, []);
+  // const handleSubmit = useEffect(() => {
+  //   axios
+  //     .post(`teams`, teamData, {
+  //       headers: {
+  //         'Content-Type': 'application/json',
+  //       },
+  //     })
+  //     .then((response) => {
+  //       console.log(response);
+  //       navigate('/TeamList');
+  //     })
+  //     .catch((error) => {
+  //       console.log(error);
+  //     });
+  // }, []);
 
-  // const onSubmit = useEffect(() => {
-  //   axios.post(`teams`, teamData, {
-  //     headers: {
-  //       'Content-Type': 'application/json',
-  //     },
-  //   });
-  // });
+  const handleChange = (e: {
+    target: {
+      team: any;
+      value: any;
+    };
+  }) => {
+    const { value } = e.target;
+    setData({
+      ...data,
+      [e.target.team]: value,
+    });
+  };
+
+  const onSubmit = (e: { preventDefault: () => void }) => {
+    e.preventDefault();
+    const teamData = {
+      name: data.name,
+      industryGroup: data.industryGroup,
+    };
+    axios.post(`teams`, teamData).then((response) => {
+      console.log(response.status, response.data);
+    });
+  };
 
   return (
     <div>
@@ -57,6 +77,54 @@ function TeamPopup() {
         팀 생성하기
       </button>
       <Modal
+        isOpen={modalIsOpen}
+        className='w-[100vw] h-[100vh] flex justify-center items-center fixed bg-[rgba(0,0,0,0.2)]'
+        contentLabel='Test Modal'
+      >
+        <form
+          onSubmit={onSubmit}
+          className='border flex flex-col justify-center items-center m-auto bg-white w-[480px] h-[600px] rounded-[30px]'
+        >
+          <label htmlFor='name'>
+            <div className='mb-4 w-[430px]'>
+              <p className='font-bold text-[20px]'>제목</p>
+              <input
+                className='text-[20px] w-[430px] border-b-2'
+                name='name'
+                onChange={() => handleChange}
+                placeholder='제목을 입력해주세요.'
+              />
+            </div>
+          </label>
+          <label htmlFor='industryGroup'>
+            <div className='mb-4 w-[430px]'>
+              <p className='font-bold text-[20px]'>부서명</p>
+              <input
+                className='text-[20px] w-[430px] border-b-2'
+                name='industryGroup'
+                onChange={() => handleChange}
+                placeholder='부서명을 입력하세요.'
+              />
+            </div>
+          </label>
+          <div className='flex space-x-2'>
+            <button
+              type='submit'
+              className='flex justify-center items-center w-[100px] h-[40px] rounded-[30px] bg-[#0075FF] text-white text-[12px]'
+            >
+              생성하기
+            </button>
+            <button
+              type='button'
+              onClick={closeModal}
+              className='flex justify-center items-center w-[100px] h-[40px] rounded-[30px] bg-[#0075FF] text-white text-[12px]'
+            >
+              닫기
+            </button>
+          </div>
+        </form>
+      </Modal>
+      {/* <Modal
         isOpen={modalIsOpen}
         className='w-[100vw] h-[100vh] flex justify-center items-center fixed bg-[rgba(0,0,0,0.2)]'
         contentLabel='Example Modal'
@@ -83,7 +151,7 @@ function TeamPopup() {
             <div className='flex space-x-2'>
               <button
                 type='button'
-                onClick={() => handleSubmit}
+                // onClick={() => handleSubmit}
                 className='flex justify-center items-center w-[100px] h-[40px] rounded-[30px] bg-[#0075FF] text-white text-[12px]'
               >
                 생성하기
@@ -98,7 +166,7 @@ function TeamPopup() {
             </div>
           </div>
         </div>
-      </Modal>
+      </Modal> */}
     </div>
   );
 }
