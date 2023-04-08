d07664bab4e96772c7700d508e6622721072d297
Choejeonghun <saa00123@naver.com>
2023-04-08 09:36:58 +0900

    feat: 초대온 목록 확인

--- front/src/components/frames/TeamList/InviteList.tsx
+++ front/src/components/frames/TeamList/InviteList.tsx
@@ -1,8 +1,20 @@
-import React, { useState } from 'react';
+import axios from 'axios';
+import React, { useState, useEffect } from 'react';
 import Modal from 'react-modal';
+
+interface List {
+  id: number;
+  nickname: string;
+  rank: string;
+  isAcceptMember: boolean;
+  isAcceptTeam: boolean;
+  name: string;
+  industryGroup: string;
+}
 
 function InviteList() {
   const [modalIsOpen, setIsOpen] = useState(false);
+  const [inviteList, setInviteList] = useState<List[]>([]);
 
   function openModal() {
     setIsOpen(true);
@@ -11,6 +23,13 @@ function InviteList() {
   function closeModal() {
     setIsOpen(false);
   }
+
+  useEffect(() => {
+    axios
+      .get<List[]>(`staffs/apply?team-id=7`)
+      .then((response) => setInviteList(response.data))
+      .catch((error) => console.log(error));
+  }, []);
 
   return (
     <div>
@@ -26,38 +45,19 @@ function InviteList() {
         className='w-[100vw] h-[100vh] flex justify-center items-center fixed bg-[rgba(0,0,0,0.2)]'
         contentLabel='Test Modal'
       >
-        <form className='border flex flex-col justify-center items-center m-auto bg-white w-[480px] h-[600px] rounded-[30px]'>
-          <label
-            htmlFor='name'
-            className='mb-4 w-[430px] font-bold text-[20px]'
-          >
-            제목
-            <input
-              className='text-[20px] w-[430px] border-b-2'
-              name='name'
-              type='name'
-              placeholder='제목을 입력해주세요.'
-            />
-          </label>
-          <label
-            htmlFor='industryGroup'
-            className='mb-4 w-[430px] font-bold text-[20px]'
-          >
-            부서명
-            <input
-              className='text-[20px] w-[430px] border-b-2'
-              name='industryGroup'
-              type='industryGroup'
-              placeholder='부서명을 입력하세요.'
-            />
-          </label>
+        <div className='border flex flex-col justify-center items-center m-auto bg-white w-[480px] h-[600px] rounded-[30px]'>
+          <div className='mb-4 w-[430px] font-bold text-[20px]'>
+            초대온 목록
+            <ul>
+              {inviteList.map((list) => (
+                <li className='text-[20px] w-[430px] border-2' key={list.id}>
+                  {list.nickname}
+                  {list.rank}
+                </li>
+              ))}
+            </ul>
+          </div>
           <div className='flex space-x-2'>
-            <button
-              type='submit'
-              className='flex justify-center items-center w-[100px] h-[40px] rounded-[30px] bg-[#0075FF] text-white text-[12px]'
-            >
-              생성하기
-            </button>
             <button
               type='button'
               onClick={closeModal}
@@ -66,7 +66,7 @@ function InviteList() {
               닫기
             </button>
           </div>
-        </form>
+        </div>
       </Modal>
     </div>
   );
