e0f42f5526c20d96764b65419a2a93239a5bf100
Choejeonghun <saa00123@naver.com>
2023-04-13 18:02:19 +0900

    test: cookie test

--- front/src/components/frames/TeamList/TeamListBox.tsx
+++ front/src/components/frames/TeamList/TeamListBox.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { useCookie } from '../Cookie/useCookie';
 import Teams from './Teams';
 import TeamPopup from '../../Popup/TeamPopup';
 import FixProfile from './FixProfile';
@@ -6,6 +7,12 @@ import InviteList from './InviteList';
 import InviteApprove from './InviteApprove';
 
 function TeamListBox() {
+  const [name, setName] = useCookie('name', '');
+
+  function handleNameChange(e: React.ChangeEvent<HTMLInputElement>) {
+    setName(e.target.value);
+  }
+
   return (
     <div className='flex flex-col justfiy-center items-center h-[100vh]'>
       <p className='font-bold text-[30px] mt-[10vh] mb-5'>CreWship</p>
@@ -14,6 +21,13 @@ function TeamListBox() {
       </div>
       <TeamPopup />
       <div className='flex flex-col mr-auto items-center space-y-2'>
+        <div>
+          <label htmlFor='name'>
+            Name:
+            <input type='text' value={name} onChange={handleNameChange} />
+          </label>
+          <p>{name}</p>
+        </div>
         <FixProfile />
         <InviteList />
         <InviteApprove />
