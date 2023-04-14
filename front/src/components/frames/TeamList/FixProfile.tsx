11c08345720d041828ba35b946fce7a788bfcb9b
Choejeonghun <saa00123@naver.com>
2023-04-14 22:49:46 +0900

    style: input style fix

--- front/src/components/frames/TeamList/FixProfile.tsx
+++ front/src/components/frames/TeamList/FixProfile.tsx
@@ -1,7 +1,6 @@
 import React, { useState } from 'react';
 import Modal from 'react-modal';
 import { useCookie } from '../Cookie/useCookie';
-import Profile from '../../../images/members.png';
 
 function FixProfile() {
   const [modalIsOpen, setIsOpen] = useState(false);
