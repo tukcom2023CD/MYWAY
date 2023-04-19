7f2329bb570f935126bcb2cfcfb71d23313d60b4
Choejeonghun <saa00123@naver.com>
2023-04-19 23:59:29 +0900

    test: style fix and pivot test

--- front/src/components/graph/SideMenu.tsx
+++ front/src/components/graph/SideMenu.tsx
@@ -7,37 +7,37 @@ export default class SideMenu extends React.Component {
     return (
       <div className='side-menu'>
         <ol>
-          <li className='tab-button'>
+          <li className='tab-button mb-2'>
             <NavLink to='/pivot-table-demo'>PIVOT TABLE DEMO</NavLink>
           </li>
         </ol>
         <div className='sub-title'>API and Events</div>
         <ol>
-          <li className='tab-button'>
+          <li className='tab-button mb-2'>
             <NavLink to='/handling-events'>HANDLING EVENTS</NavLink>
           </li>
-          <li className='tab-button'>
+          <li className='tab-button mb-2'>
             <NavLink to='/using-api-calls'>USING API CALLS</NavLink>
           </li>
-          <li className='tab-button'>
+          <li className='tab-button mb-2'>
             <NavLink to='/updating-data'>UPDATING DATA</NavLink>
           </li>
         </ol>
-        <div className='sub-title'>Customization</div>
+        <div className='sub-title mb-2'>Customization</div>
         <ol>
-          <li className='tab-button'>
+          <li className='tab-button mb-2'>
             <NavLink to='/customize-toolbar'>CUSTOMIZING THE TOOLBAR</NavLink>
           </li>
-          <li className='tab-button'>
+          <li className='tab-button mb-2'>
             <NavLink to='/customize-grid'>CUSTOMIZING THE GRID</NavLink>
           </li>
         </ol>
         <div className='sub-title'>Integration</div>
         <ol>
-          <li className='tab-button'>
+          <li className='tab-button mb-2'>
             <NavLink to='/with-highcharts'>WITH HIGHCHARTS</NavLink>
           </li>
-          <li className='tab-button'>
+          <li className='tab-button mb-2'>
             <NavLink to='/with-amcharts'>WITH AMCHARTS</NavLink>
           </li>
         </ol>
