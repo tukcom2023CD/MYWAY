f6888f75a7905059f233df7bfc97066e21272553
Choejeonghun <saa00123@naver.com>
2023-04-26 12:22:27 +0900

    fix: interface fix

--- front/src/components/frames/TeamList/Teams.tsx
+++ front/src/components/frames/TeamList/Teams.tsx
@@ -1,25 +1,36 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
-interface TeamData {
-  id: number;
-  nickname: string;
-  rank: string;
-  isAcceptMember: boolean;
-  isAcceptTeam: boolean;
+interface Member {
+  id: string;
+  name: string;
+}
+
+interface Team {
+  id: string;
   name: string;
   industryGroup: string;
 }
 
+interface TeamData {
+  id: number;
+  rank: string;
+  isAcceptMember: boolean;
+  isAcceptTeam: boolean;
+  nickname: string;
+  member: Member;
+  team: Team;
+}
+
 function Teams() {
-  const [teamData, setTeamData] = useState<TeamData[] | null>();
+  const [teamData, setTeamData] = useState<TeamData[]>();
 
   useEffect(() => {
     async function fetchData(): Promise<void> {
       try {
         const response = await axios.get<TeamData[]>('staffs/myteam');
         setTeamData(response.data);
-        console.log(response.data);
+        console.log(response.data.map);
       } catch (error) {
         console.log(error);
       }
@@ -31,12 +42,12 @@ function Teams() {
     <div>
       <ul className='w-[500px] h-[50px]'>
         {teamData
-          ? teamData.map((team) => (
+          ? teamData.map((teamData) => (
               <li
                 className='border flex space-x-2 justify-start items-center w-[500px] h-[50px]'
-                key={team.name}
+                key={teamData.id}
               >
-                {team.name}
+                {teamData.team.name}
               </li>
             ))
           : null}
