46e0afd92ca469f7f70a1942092a5864192f965f
Choejeonghun <saa00123@naver.com>
2023-05-01 14:09:53 +0900

    style: login page

---
+++ front/src/components/frames/Login/LoginHeader.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function LoginHeader() {
+  return (
+    <div className='flex justify-center items-center w-full p-5 bg-[#404040] text-white'>
+      <div className='flex'>
+        <p className='text-[20px] font-bold'>CreWship</p>
+        <div />
+      </div>
+    </div>
+  );
+}
+
+export default LoginHeader;
