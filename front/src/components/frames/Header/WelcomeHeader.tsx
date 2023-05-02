e23d18d50662719eb30d3478227609d02d00fe1e
Choejeonghun <saa00123@naver.com>
2023-05-02 23:06:24 +0900

    style: header

--- front/src/components/frames/Header/WelcomeHeader.tsx
+++ front/src/components/frames/Header/WelcomeHeader.tsx
@@ -4,16 +4,16 @@ import login from '../../../images/login.png';
 
 function WelcomeHeader() {
   return (
-    <div className='fixed border w-full p-5 bg-white'>
+    <div className='fixed border w-full p-5 bg-[#404040] text-white'>
       <div className='flex justify-around'>
         <p className='text-[20px] font-bold'>CreWship</p>
         <div />
         <NavLink
           className='flex justify-center items-center w-[100px] h-[40px] rounded-[16px]'
-          to='/Login'
+          to='/Logout'
         >
           <img className='w-[24px] h-[24px] mr-2' alt='login' src={login} />
-          <span>LOGIN</span>
+          <span>Logout</span>
         </NavLink>
       </div>
     </div>
