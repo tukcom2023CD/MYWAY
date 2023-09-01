ff9d2efaded317f5df88110b52095e051ce9e0a2
Choejeonghun <saa00123@naver.com>
2023-09-01 21:42:04 +0900

    feat: contribute data

---
+++ front/src/components/graph/GraphData/subData1.tsx
@@ -0,0 +1,29 @@
+const subData1 = [
+  {
+    subject: "커뮤니케이션",
+    A: 330,
+    fullMark: 700,
+  },
+  {
+    subject: "위기 대처",
+    A: 30,
+    fullMark: 700,
+  },
+  {
+    subject: "개발",
+    A: 630,
+    fullMark: 700,
+  },
+  {
+    subject: "리팩터링",
+    A: 240,
+    fullMark: 700,
+  },
+  {
+    subject: "기획",
+    A: 300,
+    fullMark: 700,
+  },
+];
+
+export default subData1;
