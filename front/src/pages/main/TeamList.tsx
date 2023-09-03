a0cbba82e32c1db612b5f7d43ec9ab08f832cf61
Choejeonghun <saa00123@naver.com>
2023-09-03 20:22:36 +0900

    test: change name

--- front/src/pages/main/TeamList.tsx
+++ front/src/pages/main/TeamList.tsx
@@ -1,7 +1,19 @@
-import React from 'react';
-import TeamListBox from '../../components/frames/TeamList/TeamListBox';
+import React, { useState, useEffect } from "react";
+import TeamListBox from "../../components/frames/TeamList/TeamListBox";
 
 function TeamList() {
+  const [nickname, setNickname] = useState("sample");
+
+  useEffect(() => {
+    const isLoginSuccessful = true;
+
+    if (isLoginSuccessful) {
+      const newNickname = "최정훈";
+      setNickname(newNickname);
+    }
+    console.log(setNickname);
+  }, []);
+
   return (
     <div className='flex flex-col justify-center items-center m-auto w-[100vw] h-[100vh]'>
       <TeamListBox />
